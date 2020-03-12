d196eaa5a07a8df01e4bda02ccc615a9a4273262
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-03-13 02:04:32 +0530

    ui updated

--- dataclip/src/app/app-routing.module.ts
+++ dataclip/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LOGINComponent} from "./components/login/login.component";
+import {SignupComponent} from "./components/signup/signup.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LOGINComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
