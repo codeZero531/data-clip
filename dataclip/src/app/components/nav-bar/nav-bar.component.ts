4e6262e95bb66362828387dd9115ebe4953c1861
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-03-22 21:51:00 +0530

    auth updated

--- dataclip/src/app/components/nav-bar/nav-bar.component.ts
+++ dataclip/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  isLogin: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.isLogin = this.authService.loggedIn();
   }
 
 }
