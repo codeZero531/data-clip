08081bcecea06dffc78f306f141fd15a9ca003b7
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-04-02 20:42:42 +0530

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -37,5 +37,31 @@ router.get('/:bucketId',verifyToken , (req, res, next) => {
 
 });
 
+// for node api package
+router.get('/node/:bucketId' , (req, res, next) => {
+    const bucketId = req.params.bucketId;
+    const userId= req.userId;
+    Table.find({ bucketId: bucketId})
+        .then(
+            result => {
+                if (result.length ===1 ) {
+                    // there is a bucket belongs to the user
+                    Bucket.findOne({bucketId: bucketId})
+
+                        .then(
+                            result => {
+                                res.status(200).json({status: 200, data: result.data, message: 'success'});
+                            }
+                        )
+                        .catch( err => {console.log(err); res.status(403).json({status: 403, message: err.message})});
+                } else {
+                    //no bucket belongs to logged user
+                    res.status(403).json({status: 403, message: 'Bucket not found'});
+                }
+            }
+        )
+        .catch(err => res.send(err));
+
+});
 
 module.exports = router;
